d3d516f105fc1f4aa2f24da592f5565a01f7d2d3
IvanoPisaniello <ivanop92@gmail.com>
2023-07-04 15:08:57 +0200

    added consolelog exercise

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+for (let i = 0; i <= 100; i++) {
+
+    if (i % 5 === 0) {
+        console.log("buzz")
+    } else if (i % 3 === 0) {
+        console.log("fizz")
+    } else if ((i % 5) % 3 === 0) {
+        console.log("fizzbuzz")
+    }
+    else {
+        console.log(i)
+    }
+}
+
+
